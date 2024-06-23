a7d7bf2215087c34ea91cb5608a0a705740a8d96
Long3112 <128303674+Long3112@users.noreply.github.com>
2024-06-23 22:27:41 +0700

    Create FoodController.java

---
+++ src/main/java/com/codegym/controller/FoodController.java
@@ -0,0 +1,40 @@
+    package com.codegym.controller;
+
+    import com.codegym.model.Food;
+    import com.codegym.service.IFoodService;
+    import com.codegym.service.impl.FoodService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.*;
+
+    import java.util.List;
+
+
+    @Controller
+    @CrossOrigin("*")
+    @RequestMapping("/foods")
+    public class FoodController {
+    @Autowired
+    private IFoodService foodService;
+        @GetMapping("")
+        public ResponseEntity<List<Food>> getAll(){
+            List<Food> list = foodService.findAll();
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        }
+        @PostMapping("")
+        public ResponseEntity<String> add(@RequestBody Food food) {
+            foodService.save(food);
+            String message = "Add success";
+            return new ResponseEntity<>(message, HttpStatus.CREATED);
+        }
+
+        @PutMapping("{id}")
+        public ResponseEntity<String> edit(@PathVariable Long id, @RequestBody Food food) {
+            food.setId(id);
+            foodService.save(food);
+            String message = "Edit success";
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
