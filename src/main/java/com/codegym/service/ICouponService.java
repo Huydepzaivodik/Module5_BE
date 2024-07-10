a4aed2ae7ce8170825ddc2cc5b9bb8c7eb2b7664
Long3112 <128303674+Long3112@users.noreply.github.com>
2024-07-10 17:17:37 +0700

    Add files via upload

---
+++ src/main/java/com/codegym/service/ICouponService.java
@@ -0,0 +1,7 @@
+package com.codegym.service;
+
+import com.codegym.model.Coupon;
+import com.codegym.model.Food;
+
+public interface ICouponService  extends IGenerateService<Coupon>{
+}
---
+++ src/main/java/com/codegym/service/IOrdersService.java
@@ -0,0 +1,6 @@
+package com.codegym.service;
+
+import com.codegym.model.Orders;
+
+public interface IOrdersService extends IGenerateService<Orders>{
+}
