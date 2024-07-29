7c8b2dc8bd56696b37ba3235bd3e4c5c3e461f83
huylim2003bn <huybu2003@gmail.com>
2024-07-29 21:41:07 +0700

    fix shopUer java va controller

--- src/main/java/com/codegym/service/impl/ShopService.java
+++ src/main/java/com/codegym/service/impl/ShopService.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class ShopService implements IShopService {
     @Autowired
     ShopRepository shopRepository;
+
     @Override
     public List<Shop> findAll() {
         return shopRepository.findAll();
@@ -18,13 +20,14 @@ public class ShopService implements IShopService {
 
     @Override
     public Shop findById(Long id) {
-        return null;
+        return shopRepository.findById(id).get();
     }
 
     @Override
     public Shop findByUserId(Long id) {
         return shopRepository.findShopByUserId(id);
     }
+
     @Override
     public void save(Shop shop) {
         shopRepository.save(shop);
